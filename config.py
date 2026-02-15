caad85c436026c7ea5a6389d769d6474bb41aef0
Barremukala Eswar <eswarbarremukala@gmail.com>
2026-02-15 16:07:15 +0530

    Fixed config for render

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
-FILE_PATH = r"D:\p1\college_app_data.xlsx"
-SECRET_KEY = "bheema_engineering_secret_key"
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+FILE_PATH = os.path.join(BASE_DIR, "college_app_data.xlsx")
+
+SECRET_KEY = "bheema_secret_key_2026"
